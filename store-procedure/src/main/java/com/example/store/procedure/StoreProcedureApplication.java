b9d485d66d09135d7c30d949ff2fc127ed30958b
jnyadav <34624863+jnyadav@users.noreply.github.com>
2024-09-02 10:30:03 +0530

    Add files via upload

---
+++ store-procedure/src/main/java/com/example/store/procedure/StoreProcedureApplication.java
@@ -0,0 +1,13 @@
+package com.example.store.procedure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreProcedureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreProcedureApplication.class, args);
+	}
+
+}
